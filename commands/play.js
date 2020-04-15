5cae18089af4b1dba0a2eeccce593d4df0ad9de9
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-15 02:20:07 -0400

    Added queue to client.js

--- commands/play.js
+++ commands/play.js
@@ -1,7 +1,21 @@
+const ytdl = require("ytdl-core");
+
 module.exports = {
     name: "play",
     description: "Play song in the channel",
     async execute(msg){
+        // try{
+            const args = msg.content.split(" ");
+            const queue = msg.client.queue;      
+            const serverQueue = msg.client.queue.get(msg.guild.id);
+            console.log(queue);
+            console.log("-----");
+            console.log(serverQueue);
 
+
+
+        // }catch (error){
+        //     msg.channel.send(error.message);
+        // }
     }
 }
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Client = require('./client/Client');
+const bot = new Client();
+
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
+
 
 Object.keys(botCommands).map(key => {
   bot.commands.set(botCommands[key].name, botCommands[key]);
