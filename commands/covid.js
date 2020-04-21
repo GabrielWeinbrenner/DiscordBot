610bf4635bf3ab91ad024f1f4a185a351cbfc44a
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-21 19:43:39 -0400

    Covid 19 data update

--- commands/covid.js
+++ commands/covid.js
@@ -10,29 +10,36 @@ module.exports = {
     execute(msg, args) {
         var data;
 
-        fetch("https://covidtracking.com/api/states")
+        fetch("https://corona.lmao.ninja/v2/states")
         .then(
             d => d.json()
         )
         .then(d => {
             d.forEach(element => {
-                if (element.state == "NJ") {
+                if (element.state == "New Jersey") {
                     data = element;
                 }
             });
-            console.log(data);
             const covidNJEmbed = { 
-                color: 3447003, 
-                thumbnail: "https://cdn.britannica.com/63/5363-050-F797D3B9/Africa.jpg",
+                color: 8388624, 
+                thumbnail: {
+                    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBvT6_MyDPiXGBI-LTY2nZ5tQhScGUKpcQNp8FAoQgkyprorOe&usqp=CAU",
+                },
                 title: ":house: Covid-19 Daily Updates for NJ :house:",
                 description: "The total cases for NJ include",
                 fields: [{
-                    name: "New Jersey Infected \n",
-                    value: `Total Infected: **${numberWithCommas(data.positive)}**`,
+                    name: "New Jersey Cases \n",
+                    value: `Total Cases: **${numberWithCommas(data.cases)}** (${numberWithCommas(data.todayCases)} increase)`,
+                    inline: true
+                },
+                {
+                    name: "New Jersey Total Deaths \n",
+                    value: `Total Deatgs: **${numberWithCommas(data.deaths)}** (${numberWithCommas(data.todayDeaths)} increase)`,
+                    inline: true
                 },
                 {
                     name: "New Jersey Total Tests \n" ,
-                    value: `Total Test Results: **${numberWithCommas(data.totalTestResults)}**`
+                    value: `Total Test Results: **${numberWithCommas(data.tests)}**`
                 }],
                 timestamp: new Date(),
             }
@@ -40,11 +47,13 @@ module.exports = {
             fetch("https://corona.lmao.ninja/v2/all")
             .then(data=>data.json())
             .then(data=>{
-                console.log(data);
                 const covidWorldEmbed = {
-                    color: 3447003,
+                    color: 8388624,
                     title: ":world_map: Covid-19 Daily Updates in the World :world_map:",
                     description: "Covid 19 statistics for the world",
+                    thumbnail: {
+                        url: "https://lh3.googleusercontent.com/proxy/A9YxWd6iZv4mqTHvri6tGEGD84oa0ZYzIwoI0zOLk1c2YeNVOsOuCHGTrH8BToCUKBVNyKLV4QwHKLuW9BtPMfpiLY8LVV-avVDPCYqGTmnPq2Z3va3S_c-fFgvzt2-CfPkmCOld6KuIp9bV8t_vXNOipgk"
+                    },
                     fields: [{
                         name: "World Infections",
                         value: "Total Infected: **" + numberWithCommas(data.cases) + 
@@ -59,8 +68,5 @@ module.exports = {
                 msg.channel.send({ embed: covidWorldEmbed })
             })
         })
-
-        // msg.reply('pong');
-        // msg.channel.send('pong');
     },
 };
